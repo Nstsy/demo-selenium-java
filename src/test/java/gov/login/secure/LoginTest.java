fda1f4e3893cee6af50e704d268d92661167e540
nstsy <nstsy@yandex.ru>
2024-10-24 13:54:42 +0300

    add test to validate login form with empty data

--- src/test/java/gov/login/secure/LoginTest.java
+++ src/test/java/gov/login/secure/LoginTest.java
@@ -1,4 +1,19 @@
 package gov.login.secure;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
+    @Test
+    public void test1() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://secure.login.gov/");
+        String buttonXpath = "//*[@id=\"new_user\"]/lg-submit-button/button";
+        By buttonBy = By.xpath(buttonXpath);
+        WebElement buttonWebElement = webDriver.findElement(buttonBy);
+        buttonWebElement.click();
+    }
 }
